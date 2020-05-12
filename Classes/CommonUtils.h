6b7270eeb8601be8ee87298438ff1daf22c602e7
yedongzan@126.com <yedongzan@126.com>
2020-05-12 16:13:44 +0800

    modify class

--- Classes/CommonUtils.h
+++ Classes/CommonUtils.h
@@ -8,14 +8,9 @@
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
-#import "LanguageSetting.h"
 NS_ASSUME_NONNULL_BEGIN
 
 @interface CommonUtils : NSObject {}
-
-+ (void) showAlertMsg:(UIViewController *)viewController title:(NSString *)title message:(NSString *)message;
-
-
 @end
 
 NS_ASSUME_NONNULL_END
--- Classes/CommonUtils.m
+++ Classes/CommonUtils.m
@@ -12,23 +12,6 @@
 
 @implementation CommonUtils
 
-+ (void) showAlertMsg:(UIViewController *)viewController title:(NSString *)title message:(NSString *)message {
-    
-    UIAlertController * alert = [UIAlertController alertControllerWithTitle : title
-                                                                    message : message
-                                                             preferredStyle : UIAlertControllerStyleAlert];
-    
-    UIAlertAction * ok = [UIAlertAction
-                          actionWithTitle:[LanguageSetting get:@"TitleButtonOK" alter:nil]
-                          style:UIAlertActionStyleDefault
-                          handler:^(UIAlertAction * action)
-                          { }];
-    
-    [alert addAction:ok];
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [viewController presentViewController:alert animated:YES completion:nil];
-    });
-    
 }
 
 @end
